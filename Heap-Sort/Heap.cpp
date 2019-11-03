a8fba6604fc55a2fac5105aea640909917c28437
Gianluca Marano <g.lucamara23@gmail.com>
2019-11-03 19:54:47 +0100

    renaming variables

--- Heap-Sort/Heap.cpp
+++ Heap-Sort/Heap.cpp
@@ -9,43 +9,43 @@
 #include "Heap.hpp"
 
 
-void Heap::heapify(std::vector<int>& input, int n, int index)
+void Heap::heapify(std::vector<int>& input, int dim, int index)
 {
    // Find largest among root, left child and right child
    int largest = index;
    int left = 2*index + 1;
    int right = 2*index + 2;
-   if (left < n && input.at(left) > input.at(largest))
+   if (left < dim && input.at(left) > input.at(largest))
      largest = left;
-   if (right < n && input.at(right) > input.at(largest))
+   if (right < dim && input.at(right) > input.at(largest))
      largest = right;
    // Swap and continue heapifying if root is not largest
    if (largest != index)
    {
      std::swap(input.at(index), input.at(largest));
-     heapify(input, n, largest);
+     heapify(input, dim, largest);
    }
 }
 
 void Heap::buildMaxHeap(std::vector<int>& input){
     // Build max heap
-    int len = static_cast<int>(input.size());
-    for (int i = len / 2 - 1; i >= 0; i--)
-      heapify(input, len, i);
+    int dim = static_cast<int>(input.size());
+    for (int index = dim / 2 - 1; index >= 0; index--)
+      heapify(input, dim, index);
 }
 
 
 // main function to do heap sort
 void Heap::heapSort(std::vector<int>& input)
 {
-    int len = static_cast<int>(input.size());
+    int dim = static_cast<int>(input.size());
     buildMaxHeap(input);
    // Heap sort
-   for (int i=len-1; i>=0; i--)
+   for (int index=dim-1; index>=0; index--)
    {
-     std::swap(input.at(0), input.at(i));
+     std::swap(input.at(0), input.at(index));
      // Heapify root element to get highest element at root again
-     heapify(input, i, 0);
+     heapify(input, index, 0);
    }
 }
 
