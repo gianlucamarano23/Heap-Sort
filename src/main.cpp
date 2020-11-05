4c12f5826326e84ade1038c6bcd87081460735b9
Gianluca Marano <g.lucamara23@gmail.com>
2020-11-05 22:17:26 +0100

    Adjusting .gitignore file

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,9 @@ int main()
     /** @brief Replaces index with all possible elements. The condition "end-i+1 >= r-index" makes sure that, by including one element at a time, we guarantee to perform all the combinations with the remaining elements at remaining positions
        */
     Heap heap;
-    std::vector<int> vect{1,12,9,5,6,10,56,98,23,43,14,65,90};
+    //std::vector<int> vect{1,12,9,5,6,10,56,98,23,43,14,65,90};
+    std::vector<int> vect{10,7,3};
+
     heap.print(vect);
     heap.heapSort(vect);
     std::cout << "Number sequence after sorting ::"<<std::endl;
